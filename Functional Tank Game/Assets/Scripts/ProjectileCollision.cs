f1c654c94541eb581d19165a9857e4570d245653
Anthony <anthonyjimenez1337@gmail.com>
2019-04-03 11:47:54 -0600

    Added game again

---
+++ Functional Tank Game/Assets/Scripts/ProjectileCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileCollision : MonoBehaviour
+{
+    public float Impactdelay = 1;
+
+    private void Update()
+    {
+        
+    }
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject, Impactdelay);
+        }
+        else if(other.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject, Impactdelay);
+        }
+        else if(other.gameObject.CompareTag("Tank"))
+        {
+            Destroy(gameObject, Impactdelay);
+        }
+
+    }
+    
+}
+
