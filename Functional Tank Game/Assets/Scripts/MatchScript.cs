9fcb0ae007539e29c9c1a390231cf793c2a4a1ab
Unknown <deduran@nmsu.edu>
2019-04-04 10:44:37 -0600

    added match script, not done yet

---
+++ Functional Tank Game/Assets/Scripts/MatchScript.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MatchScript : MonoBehaviour
+{
+
+    public float matchTime;
+    private int displayTime;
+    public Text timeText;
+    public float startTime;
+    private float startTrigger = 0f;
+
+    public PlayerTank1 player1;
+    public PlayerTank2 player2;
+    public Text winText;
+
+    public float disableTimeText;
+    public float returnTiTitle;
+
+    public Animator animate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        displayTime = (int)matchTime;
+        timerText.text = displayTime.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        startTrigger += Time.deltaTime;
+
+        if (startTrigger >= startTime)
+        {
+            if (player1.currentHealth > 0 && player2.currentHealth > 0)
+            {
+                matchTime -= Time.deltaTime;
+                displayTime = (int)matchTime;
+                timerText.text = displayTime.ToString();
+
+                if (fightTime < 0)
+                {
+                    player1.enabled = false;
+                    player2.enabled = false;
+                    timerText.text = "0";
+                    if (fightTime <= disableTimeText)
+                    {
+                        if (player1.currentHealth > player2.currentHealth)
+                        {
+                            winText.text = "Player 1 Wins!";
+                        }
+                        else if (player1.currentHealth < player2.currentHealth)
+                        {
+                            winText.text = "Player 2 Wins!";
+                        }
+                        else if (player1.currentHealth == player2.currentHealth)
+                        {
+                            winText.text = "Draw!";
+                        }
+                    }
+                    else
+                    {
+                        winText.text = "Time!";
+                    }
+                }
+
+            }
+            else
+            {
+                player1.enabled = false;
+                player2.enabled = false;
+                if (player1.currentHealth <= 0 && player2.currentHealth > 0)
+                {
+                    winText.text = "Player 2 Wins!";
+                }
+                else if (player2.currentHealth <= 0 && player1.currentHealth > 0)
+                {
+                    winText.text = "Player 1 Wins!";
+                }
+                else if (player1.currentHealth <= 0 && player2.currentHealth <= 0)
+                {
+                    winText.text = "Draw!";
+                }
+            }
+        }
+
+        if (matchTime <= (returnToTitle))
+        {
+            //animate.SetBool("Exit", true);
+            SceneManager.LoadScene(sceneBuildIndex: 0);
+
+        }
+
+    }
+}
